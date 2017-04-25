47d54a2d6417ebe4e29380b6fe6c6deeee046f47
Nipoon Patel <patel_nipoon@yahoo.co.nz>
2017-04-26 11:37:47 +1200

    First Push

---
+++ NodeJS/Server/server.js
@@ -0,0 +1,49 @@
+/**
+ * Created by nipoonnarendra.TRN on 1/2/2017.
+ */
+var http = require("http");
+var fs = require("fs");
+
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+
+var configJSON;
+var logger;
+
+
+app.post('/sensorReadings', function (request,response) {
+    logger.info("/sensorReadings POST was called");
+    response.send("/sensorReadings POST was called");
+});
+
+app.get('/sensorReadings', function (request,response) {
+    logger.info("/sensorReadings GET was called");
+    response.send("/sensorReadings GET was called");
+});
+
+app.listen(8099, function () {
+
+
+    var path = require('path');
+    var configPath = './public/config/config.json';
+    var correctedPath = path.normalize(configPath);
+    configJSON = require(__dirname + path.sep + correctedPath);
+
+
+    var mkdirp = require('mkdirp');
+    mkdirp(__dirname + path.sep + 'logs', function (err) {
+        if (err) console.error(err)
+        else console.log('pow!')
+    });
+
+    var log4js = require('log4js');
+    log4js.loadAppender(configJSON.log4js.loadAppenderType);
+    log4js.addAppender(log4js.appenders.file(configJSON.log4js.path), configJSON.log4js.loggerName);
+    logger = log4js.getLogger(configJSON.log4js.loggerName);
+    logger.setLevel(configJSON.log4js.level);
+
+
+    logger.info('Server running...');
+    logger.info(configJSON);
+});
