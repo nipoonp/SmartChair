340ec8fc90bd448387d55385e1ccf65a485aa5f5
Nipoon Patel <patel_nipoon@yahoo.co.nz>
2017-04-27 15:39:37 +1200

    Added store to mongoDB

--- NodeJS/Server/server.js
+++ NodeJS/Server/server.js
@@ -9,13 +9,16 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+
 var configJSON;
 var logger;
 
 // parse application/json
 app.use(bodyParser.json())
 
-var inJSON;
+inJSON = {"s0" : 0, "s1" : 0, "s2" : 0, "s3" : 0, "s4" : 0};
 
 app.post('/sensorReadings/:s0/:s1/:s2/:s3/:s4', function (request,response) {
     var data = request.params;
@@ -25,12 +28,38 @@ app.post('/sensorReadings/:s0/:s1/:s2/:s3/:s4', function (request,response) {
     var s3 = data.s3;
     var s4 = data.s4;
 
-
-	inJSON = { "s0" : s0, "s1" : s1, "s2" : s2, "s3" : s3, "s4" : s4}
-
+	inJSON = {"s0" : s0, "s1" : s1, "s2" : s2, "s3" : s3, "s4" : s4};
 
 
+    //We need to work with "MongoClient" interface in order to connect to a mongodb server.
+    var MongoClient = mongodb.MongoClient;
 
+    // Connection URL. This is where your mongodb server is running.
+    var url = configJSON.database.mongodbURL + configJSON.database.databaseName;
+
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function (err, db) {
+        if (err) {
+            logger.error('Unable to connect to the mongoDB server. Error:', err);
+        } else {
+            //HURRAY!! We are connected. :)
+            logger.info('Connection established to', url);
+
+            // Get the documents collection
+            var collection = db.collection(configJSON.database.collectionName);
+
+            // Insert some users
+            collection.insert([JSON.parse(inJSON)], function (err, result) {
+                if (err) {
+                    logger.error(err);
+                } else {
+                    logger.info('Inserted new reading!');
+                }
+                //Close connection
+                db.close();
+            });
+        }
+    });
 
 
 
@@ -46,7 +75,8 @@ app.post('/sensorReadings/:s0/:s1/:s2/:s3/:s4', function (request,response) {
 
 app.get('/sensorReadings', function (request,response) {
     logger.info("/sensorReadings GET was called");
-    response.send(inJSON);
+    //response.setHeader('Content-Type', 'application/json');
+    response.json(inJSON);
 });
 
 app.listen(8099, function () {
